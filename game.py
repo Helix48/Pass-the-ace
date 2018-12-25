7ac1a11bb736dd640ba6bada30b72d16474a45fb
Helix48 <sethjhammell@gmail.com>
2018-12-24 22:56:29 -0500

    test 5

---
+++ game.py
@@ -0,0 +1,18 @@
+from array import array
+
+print("Enter number of players")
+
+nop = input()
+
+deck = []
+
+for i in range(13):
+    deck.append(4)
+
+players = []
+
+for i in range(nop)
+    players.append()
+
+print(deck)
+
